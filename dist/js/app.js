f643ac99b699865921493366f2780b47f00fbc21
cassandradesro <cdesro10@gmail.com>
2018-08-14 15:44:32 -0400

    Adding Vue

---
+++ dist/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello World from app.js! Crystal Teams Project co-developed by Erin Kahn, \n\tCassandra Desrosiers, and Kat Sudakova. Designed by Gwynn Prewitt');
+
+var app = new Vue({
+  el: '#app',
+  router: router,
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+//# sourceMappingURL=app.js.map
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+console.log(`Hello World from app.js! Crystal Teams Project co-developed by Erin Kahn, 
+	Cassandra Desrosiers, and Kat Sudakova. Designed by Gwynn Prewitt`)
+
+var app = new Vue({
+  el: '#app',
+  router: router,
+  data: {
+    message: 'Hello Vue!'
+  }
+})
