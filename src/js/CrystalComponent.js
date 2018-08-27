1f9b6170ca294af43824b7a833ce87e04679f40e
cassandradesro <cdesro10@gmail.com>
2018-08-27 13:44:06 -0400

    crystal page designed (need mobile)

--- src/js/CrystalComponent.js
+++ src/js/CrystalComponent.js
@@ -13,7 +13,7 @@ var CrystalComponent = Vue.component("crystal-page", {
 				<h2>the "lorem ipsum"</h2>
 				<div class="chakra-area">
 					<h3>Sahasrara</h3>
-					<img class="chakra-shape" src="" alt="chakra shape">
+					<img class="chakra-shape" src="dist/img/chakras/crown-chakra.png" alt="chakra shape">
 					<h4>Crown Chakra</h4>
 				</div>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore temporibus officiis similique nihil voluptatum sapiente qui.</p>
@@ -21,19 +21,19 @@ var CrystalComponent = Vue.component("crystal-page", {
 
 			<div class="column-2">
 				<div class="numbers">
-					<img class="number number-one" src="" alt="number one">
+					<img class="active number number-one" src="dist/img/numbers/number-1.png" alt="number one">
 					<img class="number number-two" src="" alt="number two">
 					<img class="number number-three" src="" alt="number three">
 					<img class="number number-four" src="" alt="number four">
 				</div>
 				<div class="crystals">
-					<img class="crystal crystal-one" src="" alt="crystal">
+					<img class="active crystal crystal-one" src="dist/img/amethyst/amethyst-rock-1.png" alt="crystal">
 					<img class="crystal crystal-two" src="" alt="crystal">
 					<img class="crystal crystal-three" src="" alt="crystal">
 					<img class="crystal crystal-four" src="" alt="crystal">
 				</div>
 				<div class="dots">
-					<div class="dot dot-one"></div>
+					<div class="active dot dot-one"></div>
 					<div class="dot dot-two"></div>
 					<div class="dot dot-three"></div>
 					<div class="dot dot-four"></div>
