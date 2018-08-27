cdd03cc7cf30d0843db1d243d25604e47d4f9365
Erin Kahn <erinnicolekahn@gmail.com>
2018-08-26 21:15:14 -0400

    home page

--- src/js/HomeComponent.js
+++ src/js/HomeComponent.js
@@ -2,54 +2,33 @@ var HomeComponent = Vue.component("home", {
 	template: `
 		<div class="home">
 
-			<h3 class="healing-crystals">Healing Crystals</h3> 
-
-			<div class="home-text">
-				<p>
-					Crystals are alive. They connect us to the earth, because they are
-					tangible, physical forms that have powerful vibrations.
-				</p>
-				<p>
-					This energy continues to connect with you when you wear them close
-					to the skin or place them in your environment. With every thought,
-					crystals pick up on your unique vibrational energy and amplify the
-					positive vibes that you're cultivating.
-				</p>
-			</div>
-
-			<div class="dotted-circle-border">
-				
-			</div>
-
-			<div class="kaleidoscope-images">
-				<img src="" class="active rose kaleidoscope">
-				<img src="" class="amethyst kaleidoscope">
-				<img src="" class="greenfluorite kaleidoscope">
-				<img src="" class="hematite kaleidoscope">
-				<img src="" class="citrine kaleidoscope">
-				<img src="" class="red kaleidoscope">
-			</div>
-
-			<div class="circles">
-				<div class="active rose circle"></div>
-				<div class="amethyst circle"></div>
-				<div class="greenfluorite circle"></div>
-				<div class="hematite circle"></div>
-				<div class="citrine circle"></div>
-				<div class="red circle"></div>
-			</div>
+			<a href="javascript:;"><img src="dist/img/bag.png" class="checkout"></a>
 			
-			<div class="crystal-titles">
-				<h1 class="active">Rose Quartz</h1>
-				<h1>Red Jasper</h1>
-				<h1>Citrine</h1>
-				<h1>Amethyst</h1>
-				<h1>Green Fluorite</h1>
-				<h1>Hematite</h1>
+			<h1 class="crystal-titles">Rose Quartz</h1>
+
+			<h3 class="healing-crystals">Healing Crystals</h3> 
+				
+			<div class="max-container">
+
+				
+
+				<div class="kaleidoscope-images">
+					<img src="dist/img/home/rose-kaleidoscope.png" class="rose kaleidoscope">
+				</div>
+
+				<div class="home-text">
+					<p>
+						Crystals are alive. They connect us to the earth, because they are
+						tangible, physical forms that have powerful vibrations.
+					</p>
+					<p>
+						This energy continues to connect with you when you wear them close
+						to the skin or place them in your environment. With every thought,
+						crystals pick up on your unique vibrational energy and amplify the
+						positive vibes that you're cultivating.
+					</p>
+				</div>
 			</div>
-
-			<a href=""><img src="" class="checkout"></a>
-
 
 			<router-link></router-link>
 
