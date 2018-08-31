ee17c2852b51103c54ee919dda279e8b812bdad8
cassandradesro <cdesro10@gmail.com>
2018-08-31 12:05:48 -0400

    fluorite added, array for crystals and numbers and facts in crystal object, trying to set up method to change Crystal on click

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,54 +3,107 @@
 console.log('Hello World from app.js! Crystal Teams Project co-developed by Erin Kahn, \n\tCassandra Desrosiers, and Kat Sudakova. Designed by Gwynn Prewitt');
 
 var crystals = {
+
 	rosequartz: {
-		title: "Rose Quartz",
+		title: 'Rose Quartz',
+		subtitle: 'the "love stone"',
+		chakraName: 'Anahata',
+		chakraNickname: 'Heart Chakra',
+		chakraImage: 'dist/img/chakras/heart-chakra.png',
+		description: 'Color varieties are usually soft, ranging from very light pink to medium pink in intensity. Rose Quartz are often milky or hazy, and may lack transparency.',
+		verticalText: 'oxide mineral',
+		facts: ['aids emotional healing', 'opens heart to give and recieve love', 'emits joy and warmth', 'encourages forgiveness', 'bestows peaceful sensations'],
+		crystalImages: ['dist/img/rose/rose-rock-1.png', 'dist/img/rose/rose-rock-2.png', 'dist/img/rose/rose-rock-3.png', 'dist/img/rose/rose-rock-4.png'],
+		backgroundTexture: 'url(dist/img/rose/rose-texture.png)',
 		homeh1Color: '#d74b62',
 		homeTextColor: '#d74b62',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: '#f6c3e0',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
 	},
 	amethyst: {
-		title: "Amethyst",
+		title: 'Amethyst',
 		subtitle: 'the "intuitive eye"',
-		chakraName: "Sahasrara",
-		chakraNickname: "Crown Chakra",
-		chakra2Name: "Sahasrara",
-		chakra2Nickname: "Crown Chakra",
-		description: "Lorem ipsum dolor sit amet, con enim, exercitationem quibusdam similique porro est voluptatem impedit deserunt natus numquam, asperiores voluptates consequuntur!",
-		verticalText: "oxide mineral",
+		chakraName: 'Sahasrara',
+		chakraNickname: 'Crown Chakra',
+		chakraImage: 'dist/img/chakras/crown-chakra.png',
+		description: 'Primary hues range from a light pinkish violet to a deep purple. Amethyst may exhibit one or both secondary hues, red and blue.',
+		verticalText: 'oxide mineral',
+		facts: ['brings intense spiritual growth', 'aids emotional healing', 'supports overcoming addiction', 'helps relieve stress and anxiety', 'stabilizes mood swings'],
+		crystalImages: ['dist/img/amethyst/amethyst-rock-1.png', 'dist/img/amethyst/amethyst-rock-2.png', 'dist/img/amethyst/amethyst-rock-3.png', 'dist/img/amethyst/amethyst-rock-4.png'],
+		backgroundTexture: 'url(dist/img/amethyst/amethyst-texture.png)',
 		homeh1Color: '#4b365a',
 		homeTextColor: '#4b365a',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: '#d1b0d8',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
+
 	},
 	citrine: {
-		title: "Citrine",
-		subtitle: 'the "intuitive eye"',
-		chakraName: "Sahasrara",
-		chakraNickname: "Crown Chakra",
-		description: "Lorem ipsum dolor sit amet, con enim, exercitationem quibusdam similique porro est voluptatem impedit deserunt natus numquam, asperiores voluptates consequuntur!",
-		verticalText: "oxide mineral",
+		title: 'Citrine',
+		subtitle: 'the "lemon quartz"',
+		chakraName: 'Manipura',
+		chakraNickname: 'Solar Plexus Chakra',
+		chakraImage: 'dist/img/chakras/solar-chakra.png',
+		description: 'Common colored varities include yellow, greenish-yellow, brownish yellow or orange.',
+		verticalText: 'silicone dioxide',
+		facts: ['carries power of sun', 'energizing, comforting and live giving', 'helps clear the mind', 'stirs the soul to seek action', 'awakens creativity and imagination'],
+		crystalImages: ['dist/img/citrine/citrine-rock-1.png', 'dist/img/citrine/citrine-rock-2.png', 'dist/img/citrine/citrine-rock-3.png', 'dist/img/citrine/citrine-rock-4.png'],
+		backgroundTexture: 'url(dist/img/citrine/citrine-texture.png)',
 		homeh1Color: '#fbc75c',
 		homeTextColor: '#fbc75c',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: '#da7713',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
 	},
 	jasper: {
-		title: "Red Jasper",
+		title: 'Red Jasper',
+		subtitle: 'the "blood of Mother Earth"',
+		chakraName: 'Muladhara',
+		chakraNickname: 'Root Chakra',
+		chakraImage: 'dist/img/chakras/root-chakra.png',
+		chakra2Name: 'Svadhishtha',
+		chakra2Nickname: 'Sacral Chakra',
+		chakra2Image: 'dist/img/chakras/sacral-chakra.png',
+		description: 'Common colored varities include red, yellow, brown, green, and rarely blue.',
+		verticalText: 'silicone dioxide',
+		facts: ['alleviates stress', 'helps reconnect with Earths grounding', 'eleminates negativity', 'ideal worry or rubbing stone', 'helps clear mind during meditation'],
+		crystalImages: ['dist/img/jasper/jasper-rock-1.png', 'dist/img/jasper/jasper-rock-2.png', 'dist/img/jasper/jasper-rock-3.png', 'dist/img/jasper/jasper-rock-4.png'],
+		backgroundTexture: 'url(dist/img/jasper/jasper-texture.png)',
 		homeh1Color: '#901b00',
 		homeTextColor: '#901b00',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: '#ff7f79',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
 
 	},
 	greenfluorite: {
-		title: "Green Fluorite",
+		title: 'Green Fluorite',
+		subtitle: 'the "blood of Mother Earth"',
+		chakraName: 'Muladhara',
+		chakraNickname: 'Root Chakra',
+		chakraImage: 'dist/img/chakras/root-chakra.png',
+		chakra2Name: 'Svadhishtha',
+		chakra2Nickname: 'Sacral Chakra',
+		chakra2Image: 'dist/img/chakras/sacral-chakra.png',
+		description: 'Common colored varities include red, yellow, brown, green, and rarely blue.',
+		verticalText: 'silicone dioxide',
+		facts: ['alleviates stress', 'helps reconnect with Earths grounding', 'eleminates negativity', 'ideal worry or rubbing stone', 'helps clear mind during meditation'],
+		crystalImages: ['dist/img/fluorite/fluorite-rock-1.png', 'dist/img/fluorite/fluorite-rock-2.png', 'dist/img/fluorite/fluorite-rock-3.png', 'dist/img/fluorite/fluorite-rock-4.png'],
+		backgroundTexture: 'url(dist/img/fluorite/fluorite-texture-01.png)',
 		homeh1Color: 'green',
 		homeTextColor: 'green',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: 'green',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
 	},
 	hematite: {
-		title: "Hematite",
+		title: 'Hematite',
 		homeh1Color: 'rgb(68, 68, 68)',
 		homeTextColor: 'rgb(68, 68, 68)',
-		homeKaleidoscope: ''
+		homeKaleidoscope: '',
+		backgroundColor: 'rgb(68, 68, 68)',
+		numberImages: ['dist/img/numbers/number-1.png', 'dist/img/numbers/number-2.png', 'dist/img/numbers/number-3.png', 'dist/img/numbers/number-4.png']
 	}
 };
 
@@ -59,12 +112,10 @@ var router = new VueRouter({
 });
 
 var app = new Vue({
-	el: "#app",
+	el: '#app',
 	router: router,
 	data: {
-		message: "Hey Y'all!",
-		color: "red",
-		cart: []
+		facts: ''
 	}
 });
 //# sourceMappingURL=app.js.map
--- src/js/CrystalComponent.js
+++ src/js/CrystalComponent.js
@@ -14,7 +14,7 @@ var CrystalComponent = Vue.component("crystal-page", {
 			<div class="column-1">
 				<h1 v-bind:style="'color: ' + crystal.homeh1Color">{{crystal.title}}</h1>
 				<h2>{{crystal.subtitle}}</h2>
-				<div class="show-on-mobile column-3">
+				<div class="hide-on-desktop column-3">
 					<div class="bullets">
 						<div class="bullet"></div>
 						<div class="bullet"></div>
@@ -22,73 +22,103 @@ var CrystalComponent = Vue.component("crystal-page", {
 						<div class="bullet"></div>
 						<div class="bullet"></div>
 					</div>
-					<!--put border-left on the .facts div then add margin-left (or padding-left) until it crosses through center of bullets-->
-					<div class="facts">
-						<p class="fact">{{crystal.fact1}}</p>
-						<p class="fact">{{crystal.fact2}}</p>
-						<p class="fact">{{crystal.fact3}}</p>
-						<p class="fact">{{crystal.fact4}}</p>
-						<p class="fact">{{crystal.fact5}}</p>
-					</div>
+					<ul class="facts">
+						<li class="fact" v-for="fact in crystal.facts">
+							{{fact}}
+						</li>
+					
+					</ul>
 				</div>
 				<div class="chakras">
 					<div class="chakra-area">
 						<h3>{{crystal.chakraName}}</h3>
-						<img class="chakra-shape" v-bind:src="crystal.chakraImage" alt="chakra shape">
+						<img class="chakra-shape" v-bind:src="crystal.chakraImage[counter]" alt="chakra shape">
 						<h4 v-bind:style="'color: ' + crystal.homeh1Color">{{crystal.chakraNickname}}</h4>
 					</div>
 					<div class="chakra-area" v-if="crystal.chakra2Name">
 						<h3>{{crystal.chakra2Name}}</h3>
-						<img class="chakra-shape" v-bind:src="crystal.chakra2Image" alt="chakra shape">
+						<img class="chakra-shape" v-bind:src="crystal.chakra2Image[counter]" alt="chakra shape">
 						<h4 v-bind:style="'color: ' + crystal.homeh1Color">{{crystal.chakra2Nickname}}</h4>
 					</div>
 				</div>
 				<p class="hide-on-mobile">{{crystal.description}}</p>
 			</div>
-
+			<div   @click="move(-1)" id="prev" class="hide-on-desktop">
+				<span v-bind:style="'color: ' + crystal.homeh1Color"></span>
+			</div>
 			<div class="column-2">
+
 				<div class="numbers">
-					<img class="active number number-one" v-bind:src="crystal.number1Image" alt="number one">
-					<img class="number number-two" v-bind:src="crystal.number2Image" alt="number two">
-					<img class="number number-three" v-bind:src=" crystal.number3Image" alt="number three">
-					<img class="number number-four" v-bind:src="crystal.number4Image" alt="number four">
+					<img class="active number slide" v-if="index == active" v-bind:src="crystal.numberImages[index]" alt="number one">
+
 				</div>
 				<div class="crystals">
-					<img class="active crystal crystal-one" v-bind:src="crystal.crystal1Image" alt="crystal">
-					<img class="crystal crystal-two" v-bind:src="crystal.crystal2Image" alt="crystal">
-					<img class="crystal crystal-three" v-bind:src="crystal.crystal3Image" alt="crystal">
-					<img class="crystal crystal-four" v-bind:src="crystal.crystal4Image" alt="crystal">
+					<img class="active crystal" v-if="index == active" v-bind:src="crystal.crystalImages[index]" alt="crystal">
+
 				</div>
-				<div class="dots">
-					<div v-bind:style="'border-color: ' + crystal.homeh1Color" class="active dot dot-one"></div>
-					<div v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot dot-two"></div>
-					<div v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot dot-three"></div>
-					<div v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot dot-four"></div>
-				</div>
+				<ul class="dots">
+					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="active dot"></div>
+					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
+					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
+					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
+				</ul>
+
 			</div> <!--function module to updates numbers, crystals and dots simultaneously-->
-					
+			<div  @click="move(1)" id="next" class="hide-on-desktop">
+				<span v-bind:style="'color: ' + crystal.homeh1Color"></span>
+			</div>		
 			<div class="column-3">
-				<p class="">{{crystal.description}}</p>
-				<div class="show-on-mobile" hide-on-mobile bullets">
+				<p class="hide-on-desktop">{{crystal.description}}</p>
+				<div class="hide-on-mobile  bullets">
 					<div class="bullet"></div>
 					<div class="bullet"></div>
 					<div class="bullet"></div>
 					<div class="bullet"></div>
 					<div class="bullet"></div>
 				</div>
-				<!--put border-left on the .facts div then add margin-left (or padding-left) until it crosses through center of bullets-->
-				<div class="show-on-mobile facts">
-					<p class="fact">{{crystal.fact1}}</p>
-					<p class="fact">{{crystal.fact2}}</p>
-					<p class="fact">{{crystal.fact3}}</p>
-					<p class="fact">{{crystal.fact4}}</p>
-					<p class="fact">{{crystal.fact5}}</p>
-				</div>
+				<ul class="hide-on-mobile facts">
+					<li class="fact" v-for="fact in crystal.facts">
+						{{fact}}
+					</li>
+					
+				</ul>
 			</div>
 
 			<div class="add-button" v-bind:style="'background-color: ' + crystal.backgroundColor">add</div> <!--position absolute-->
 			
 		</section>
 	`
-
+	data:{
+		slides: 5,
+		active: 1,
+	},
+	methods: {
+	    move(amount) {
+	      let newActive
+	      const newIndex = this.active + amount
+	      if (newIndex > this.slides) newActive = 1
+	      if (newIndex === 0) newActive = this.slides
+	      this.active = newActive || newIndex
+	    },
+	    jump(index) {
+	      this.active = index
+	    },
+	}
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
