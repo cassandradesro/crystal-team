e6ed70fcc58673fa2f365a63e820d8af10b271d5
ekaterinasudakova <32370845+ekaterinasudakova@users.noreply.github.com>
2018-09-04 15:11:21 -0400

    crystals initial

--- dist/js/CrystalComponent.js
+++ dist/js/CrystalComponent.js
@@ -3,7 +3,73 @@
 console.log();
 var CrystalComponent = Vue.component("crystal-page", {
 	props: ['crystal'],
-	template: "\n\t\t<section class=\"page\">\n\t\t\t<!--display flex on .page, with flex-direction: row-->\n\n\t\t\t<img class=\"bag-icon\" src=\"dist/img/bag.png\" alt=\"shopping bag icon\"> <!--position absolute-->\n\t\t\t<p class=\"vertical-text\">oxide mineral</p> <!--position absolute-->\n\n\t\t\t<div class=\"column-1\">\n\t\t\t\t<h1>{{crystal.title}}</h1>\n\t\t\t\t<h2>the \"lorem ipsum\"</h2>\n\t\t\t\t<div class=\"chakra-area\">\n\t\t\t\t\t<h3>Sahasrara</h3>\n\t\t\t\t\t<img class=\"chakra-shape\" src=\"dist/img/chakras/crown-chakra.png\" alt=\"chakra shape\">\n\t\t\t\t\t<h4>Crown Chakra</h4>\n\t\t\t\t</div>\n\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore temporibus officiis similique nihil voluptatum sapiente qui.</p>\n\t\t\t</div>\n\n\t\t\t<div class=\"column-2\">\n\t\t\t\t<div class=\"numbers\">\n\t\t\t\t\t<img class=\"active number number-one\" src=\"dist/img/numbers/number-1.png\" alt=\"number one\">\n\t\t\t\t\t<img class=\"number number-two\" src=\"\" alt=\"number two\">\n\t\t\t\t\t<img class=\"number number-three\" src=\"\" alt=\"number three\">\n\t\t\t\t\t<img class=\"number number-four\" src=\"\" alt=\"number four\">\n\t\t\t\t</div>\n\t\t\t\t<div class=\"crystals\">\n\t\t\t\t\t<img class=\"active crystal crystal-one\" src=\"dist/img/amethyst/amethyst-rock-1.png\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-two\" src=\"\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-three\" src=\"\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-four\" src=\"\" alt=\"crystal\">\n\t\t\t\t</div>\n\t\t\t\t<div class=\"dots\">\n\t\t\t\t\t<div class=\"active dot dot-one\"></div>\n\t\t\t\t\t<div class=\"dot dot-two\"></div>\n\t\t\t\t\t<div class=\"dot dot-three\"></div>\n\t\t\t\t\t<div class=\"dot dot-four\"></div>\n\t\t\t\t</div>\n\t\t\t</div> <!--function module to updates numbers, crystals and dots simultaneously-->\n\t\t\t\t\t\n\t\t\t<div class=\"column-3\">\n\t\t\t\t<div class=\"bullets\">\n\t\t\t\t\t<div class=\"bullet\"></div>\n\t\t\t\t\t<div class=\"bullet\"></div>\n\t\t\t\t\t<div class=\"bullet\"></div>\n\t\t\t\t\t<div class=\"bullet\"></div>\n\t\t\t\t\t<div class=\"bullet\"></div>\n\t\t\t\t</div>\n\t\t\t\t<!--put border-left on the .facts div then add margin-left (or padding-left) until it crosses through center of bullets-->\n\t\t\t\t<div class=\"facts\">\n\t\t\t\t\t<p class=\"fact\">Lorem ipsum donum ab t. Sit, voluptate!</p>\n\t\t\t\t\t<p class=\"fact\">Lorem ipsum doloppe offrem?</p>\n\t\t\t\t\t<p class=\"fact\">Loreues comesse facilis aliquam neque!</p>\n\t\t\t\t\t<p class=\"fact\">Lorem ipsum dla fugaptatem repellendus porro.</p>\n\t\t\t\t\t<p class=\"fact\">Lorem ipsum doloaonsectmagni doloribus deserunt.</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\n\t\t\t<div class=\"add-button\">add</div> <!--position absolute-->\n\t\t\t\n\t\t</section>\n\t"
+	template: "\n\t\t<section class=\"page\">\n\t\t\t<!--display flex on .page, with flex-direction: row-->\n\n\t\t\t<img ref=\"checkoutCRef\" class=\"bag-icon\" src=\"dist/img/bag.png\" alt=\"shopping bag icon\"> <!--position absolute-->\n\t\t\t<p ref=\"logoCRef\" class=\"vertical-text\">oxide mineral</p> <!--position absolute-->\n\n\t\t\t<div ref=\"C1Ref\" class=\"column-1\">\n\t\t\t\t<h1 ref=\"titleC1Ref\">{{crystal.title}}</h1>\n\t\t\t\t<h2 ref=\"titleTwoC1Ref\">the \"lorem ipsum\"</h2>\n\t\t\t\t<div ref=\"chakraC1Ref\" class=\"chakra-area\">\n\t\t\t\t\t<h3>Sahasrara</h3>\n\t\t\t\t\t<img class=\"chakra-shape\" src=\"dist/img/chakras/crown-chakra.png\" alt=\"chakra shape\">\n\t\t\t\t\t<h4>Crown Chakra</h4>\n\t\t\t\t</div>\n\t\t\t\t<p ref=\"paraC1Ref\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore temporibus officiis similique nihil voluptatum sapiente qui.</p>\n\t\t\t</div>\n\n\t\t\t<div class=\"column-2\">\n\t\t\t\t<div ref=\"numberC2Ref\" class=\"numbers\">\n\t\t\t\t\t<img class=\"active number number-one\" src=\"dist/img/numbers/number-1.png\" alt=\"number one\">\n\t\t\t\t\t<img class=\"number number-two\" src=\"\" alt=\"number two\">\n\t\t\t\t\t<img class=\"number number-three\" src=\"\" alt=\"number three\">\n\t\t\t\t\t<img class=\"number number-four\" src=\"\" alt=\"number four\">\n\t\t\t\t</div>\n\t\t\t\t<div ref=\"imgC2Ref\" class=\"crystals\">\n\t\t\t\t\t<img class=\"active crystal crystal-one\" src=\"dist/img/amethyst/amethyst-rock-1.png\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-two\" src=\"\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-three\" src=\"\" alt=\"crystal\">\n\t\t\t\t\t<img class=\"crystal crystal-four\" src=\"\" alt=\"crystal\">\n\t\t\t\t</div>\n\t\t\t\t<div ref=\"dotsC2Ref\" class=\"dots\">\n\t\t\t\t\t<div class=\"active dot dot-one\"></div>\n\t\t\t\t\t<div class=\"dot dot-two\"></div>\n\t\t\t\t\t<div class=\"dot dot-three\"></div>\n\t\t\t\t\t<div class=\"dot dot-four\"></div>\n\t\t\t\t</div>\n\t\t\t</div> <!--function module to updates numbers, crystals and dots simultaneously-->\n\t\t\t\t\t\n\t\t\t<div ref=\"C3Ref\" class=\"column-3\">\n\t\t\t\t<div class=\"bullets\">\n\t\t\t\t\t<div ref=\"d1C3Ref\" class=\"bullet\"></div>\n\t\t\t\t\t<div ref=\"d2C3Ref\" class=\"bullet\"></div>\n\t\t\t\t\t<div ref=\"d3C3Ref\" class=\"bullet\"></div>\n\t\t\t\t\t<div ref=\"d4C3Ref\" class=\"bullet\"></div>\n\t\t\t\t\t<div ref=\"d5C3Ref\" class=\"bullet\"></div>\n\t\t\t\t</div>\n\t\t\t\t<!--put border-left on the .facts div then add margin-left (or padding-left) until it crosses through center of bullets-->\n\t\t\t\t<div ref=\"factsC3Ref\" class=\"facts\">\n\t\t\t\t\t<p ref=\"f1C3Ref\" class=\"fact\">Lorem ipsum donum ab t. Sit, voluptate!</p>\n\t\t\t\t\t<p ref=\"f2C3Ref\" class=\"fact\">Lorem ipsum doloppe offrem?</p>\n\t\t\t\t\t<p ref=\"f3C3Ref\" class=\"fact\">Loreues comesse facilis aliquam neque!</p>\n\t\t\t\t\t<p ref=\"f4C3Ref\" class=\"fact\">Lorem ipsum dla fugaptatem repellendus porro.</p>\n\t\t\t\t\t<p ref=\"f5C3Ref\" class=\"fact\">Lorem ipsum doloaonsectmagni doloribus deserunt.</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\n\t\t\t<div ref=\"addCRef\" class=\"add-button\" onclick='addItem()'>add</div> <!--position absolute-->\n\t\t\t\n\t\t</section>\n\t",
+	mounted: function mounted() {
+		console.log("right when the component is created");
+		console.log(this.$refs.numberCrystRef);
+		console.log(this.$refs.kaleidoImgsRef);
+		var numberC2 = this.$refs.numberC2Ref;
+		var imgC2 = this.$refs.imgC2Ref;
+		var dotsC2 = this.$refs.dotsC2Ref;
+
+		var titleC1 = this.$refs.titleC1Ref;
+		var title2C1 = this.$refs.titleTwoC1Ref;
+		var chakraC1 = this.$refs.chakraC1Ref;
+		var paraC1 = this.$refs.paraC1Ref;
+
+		var d1C3 = this.$refs.d1C3Ref;
+		var d2C3 = this.$refs.d2C3Ref;
+		var d3C3 = this.$refs.d3C3Ref;
+		var d4C3 = this.$refs.d4C3Ref;
+		var d5C3 = this.$refs.d5C3Ref;
+
+		var factsC3 = this.$refs.factsC3Ref;
+
+		var f1C3 = this.$refs.f1C3Ref;
+		var f2C3 = this.$refs.f2C3Ref;
+		var f3C3 = this.$refs.f3C3Ref;
+		var f4C3 = this.$refs.f4C3Ref;
+		var f5C3 = this.$refs.f5C3Ref;
+
+		var C1 = this.$refs.C1Ref;
+		var C3 = this.$refs.C3Ref;
+
+		var logoC = this.$refs.logoCRef;
+		var checkoutC = this.$refs.checkoutCRef;
+		var addC = this.$refs.addCRef;
+
+		var tl = new TimelineMax();
+
+		tl.from(numberC2, 0.3, { y: -15, autoAlpha: 0, ease: Power1.easeOut }, '+=0.25').from(imgC2, 0.8, { rotation: 220, transformOrigin: '50% 50%', y: -700, autoAlpha: 0, ease: Power1.easeOut }, '+=0.15').add('imgs-in').from(titleC1, 0.1, { y: -15, autoAlpha: 0, ease: Power1.easeOut }, 'imgs-in')
+		// .from(title2C1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .from(chakraC1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .from(paraC1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+
+		.from(C1, 0.2, { x: -15, autoAlpha: 0, ease: Power1.easeOut }, '+=0.2').from(C3, 0.2, { x: 15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.1')
+
+		// .from(factsC3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.35')
+
+		// .from(d1C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .add('d1')
+		// .from(d2C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .add('d2')
+		// .from(d3C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .add('d3')
+		// .from(d4C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .add('d4')
+		// .from(d5C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+		// .add('d5')
+
+		// .from(f1C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d1-=0.15')
+		// .from(f2C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d2-=0.15')
+		// .from(f3C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d3-=0.15')
+		// .from(f4C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d4-=0.15')
+		// .from(f5C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d5-=0.15')
+
+
+		.from(logoC, 0.2, { x: -15, autoAlpha: 0, ease: Power1.easeOut }, '+=0.50').add('sides1').from(checkoutC, 0.2, { y: -15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.2').add('sides2').from(dotsC2, 0.2, { y: 15, autoAlpha: 0, ease: Power1.easeOut }, 'sides2').from(addC, 0.2, { y: 15, autoAlpha: 0, ease: Power1.easeOut }, 'sides2');
+	},
+	methods: function addItem() {}
 
 });
 //# sourceMappingURL=CrystalComponent.js.map
--- src/js/CrystalComponent.js
+++ src/js/CrystalComponent.js
@@ -5,34 +5,34 @@ var CrystalComponent = Vue.component("crystal-page", {
 		<section class="page">
 			<!--display flex on .page, with flex-direction: row-->
 
-			<img class="bag-icon" src="dist/img/bag.png" alt="shopping bag icon"> <!--position absolute-->
-			<p class="vertical-text">oxide mineral</p> <!--position absolute-->
+			<img ref="checkoutCRef" class="bag-icon" src="dist/img/bag.png" alt="shopping bag icon"> <!--position absolute-->
+			<p ref="logoCRef" class="vertical-text">oxide mineral</p> <!--position absolute-->
 
-			<div class="column-1">
-				<h1>{{crystal.title}}</h1>
-				<h2>the "lorem ipsum"</h2>
-				<div class="chakra-area">
+			<div ref="C1Ref" class="column-1">
+				<h1 ref="titleC1Ref">{{crystal.title}}</h1>
+				<h2 ref="titleTwoC1Ref">the "lorem ipsum"</h2>
+				<div ref="chakraC1Ref" class="chakra-area">
 					<h3>Sahasrara</h3>
 					<img class="chakra-shape" src="dist/img/chakras/crown-chakra.png" alt="chakra shape">
 					<h4>Crown Chakra</h4>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore temporibus officiis similique nihil voluptatum sapiente qui.</p>
+				<p ref="paraC1Ref">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore temporibus officiis similique nihil voluptatum sapiente qui.</p>
 			</div>
 
 			<div class="column-2">
-				<div class="numbers">
+				<div ref="numberC2Ref" class="numbers">
 					<img class="active number number-one" src="dist/img/numbers/number-1.png" alt="number one">
 					<img class="number number-two" src="" alt="number two">
 					<img class="number number-three" src="" alt="number three">
 					<img class="number number-four" src="" alt="number four">
 				</div>
-				<div class="crystals">
+				<div ref="imgC2Ref" class="crystals">
 					<img class="active crystal crystal-one" src="dist/img/amethyst/amethyst-rock-1.png" alt="crystal">
 					<img class="crystal crystal-two" src="" alt="crystal">
 					<img class="crystal crystal-three" src="" alt="crystal">
 					<img class="crystal crystal-four" src="" alt="crystal">
 				</div>
-				<div class="dots">
+				<div ref="dotsC2Ref" class="dots">
 					<div class="active dot dot-one"></div>
 					<div class="dot dot-two"></div>
 					<div class="dot dot-three"></div>
@@ -40,27 +40,110 @@ var CrystalComponent = Vue.component("crystal-page", {
 				</div>
 			</div> <!--function module to updates numbers, crystals and dots simultaneously-->
 					
-			<div class="column-3">
+			<div ref="C3Ref" class="column-3">
 				<div class="bullets">
-					<div class="bullet"></div>
-					<div class="bullet"></div>
-					<div class="bullet"></div>
-					<div class="bullet"></div>
-					<div class="bullet"></div>
+					<div ref="d1C3Ref" class="bullet"></div>
+					<div ref="d2C3Ref" class="bullet"></div>
+					<div ref="d3C3Ref" class="bullet"></div>
+					<div ref="d4C3Ref" class="bullet"></div>
+					<div ref="d5C3Ref" class="bullet"></div>
 				</div>
 				<!--put border-left on the .facts div then add margin-left (or padding-left) until it crosses through center of bullets-->
-				<div class="facts">
-					<p class="fact">Lorem ipsum donum ab t. Sit, voluptate!</p>
-					<p class="fact">Lorem ipsum doloppe offrem?</p>
-					<p class="fact">Loreues comesse facilis aliquam neque!</p>
-					<p class="fact">Lorem ipsum dla fugaptatem repellendus porro.</p>
-					<p class="fact">Lorem ipsum doloaonsectmagni doloribus deserunt.</p>
+				<div ref="factsC3Ref" class="facts">
+					<p ref="f1C3Ref" class="fact">Lorem ipsum donum ab t. Sit, voluptate!</p>
+					<p ref="f2C3Ref" class="fact">Lorem ipsum doloppe offrem?</p>
+					<p ref="f3C3Ref" class="fact">Loreues comesse facilis aliquam neque!</p>
+					<p ref="f4C3Ref" class="fact">Lorem ipsum dla fugaptatem repellendus porro.</p>
+					<p ref="f5C3Ref" class="fact">Lorem ipsum doloaonsectmagni doloribus deserunt.</p>
 				</div>
 			</div>
 
-			<div class="add-button">add</div> <!--position absolute-->
+			<div ref="addCRef" class="add-button" onclick='addItem()'>add</div> <!--position absolute-->
 			
 		</section>
-	`
+	`,
+	mounted: function() {
+		console.log("right when the component is created");
+		console.log( this.$refs.numberCrystRef );
+		console.log( this.$refs.kaleidoImgsRef );
+		let numberC2 = this.$refs.numberC2Ref;
+		let imgC2 = this.$refs.imgC2Ref;
+		let dotsC2 = this.$refs.dotsC2Ref;
+
+		let titleC1 = this.$refs.titleC1Ref;
+		let title2C1 = this.$refs.titleTwoC1Ref;
+		let chakraC1 = this.$refs.chakraC1Ref;
+		let paraC1 = this.$refs.paraC1Ref;
+
+		let d1C3 = this.$refs.d1C3Ref;
+		let d2C3 = this.$refs.d2C3Ref;
+		let d3C3 = this.$refs.d3C3Ref;
+		let d4C3 = this.$refs.d4C3Ref;
+		let d5C3 = this.$refs.d5C3Ref;
+
+		let factsC3 = this.$refs.factsC3Ref;
+
+		let f1C3 = this.$refs.f1C3Ref;
+		let f2C3 = this.$refs.f2C3Ref;
+		let f3C3 = this.$refs.f3C3Ref;
+		let f4C3 = this.$refs.f4C3Ref;
+		let f5C3 = this.$refs.f5C3Ref;
+
+		let C1 = this.$refs.C1Ref;
+		let C3 = this.$refs.C3Ref;
+
+		let logoC = this.$refs.logoCRef;
+		let checkoutC = this.$refs.checkoutCRef;
+		let addC = this.$refs.addCRef;
+	
+		
+	
+		let tl = new TimelineMax();
+
+		tl
+			.from(numberC2, 0.3, {y:-15, autoAlpha:0, ease:Power1.easeOut}, '+=0.25')
+			.from(imgC2, 0.8, {rotation: 220, transformOrigin: '50% 50%', y:-700, autoAlpha:0, ease:Power1.easeOut}, '+=0.15')
+			.add('imgs-in')
+
+			.from(titleC1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'imgs-in')
+			// .from(title2C1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .from(chakraC1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .from(paraC1, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			
+			.from(C1, 0.2, {x:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.2')
+			.from(C3, 0.2, {x:15, autoAlpha:0, ease:Power1.easeOut},'-=0.1')
+
+			// .from(factsC3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.35')
+
+			// .from(d1C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .add('d1')
+			// .from(d2C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .add('d2')
+			// .from(d3C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .add('d3')
+			// .from(d4C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .add('d4')
+			// .from(d5C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.15')
+			// .add('d5')
+
+			// .from(f1C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d1-=0.15')
+			// .from(f2C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d2-=0.15')
+			// .from(f3C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d3-=0.15')
+			// .from(f4C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d4-=0.15')
+			// .from(f5C3, 0.1, {y:-15, autoAlpha:0, ease:Power1.easeOut},'d5-=0.15')
+
+			
+			
+			.from(logoC, 0.2, {x:-15, autoAlpha:0, ease:Power1.easeOut},'+=0.50')
+			.add('sides1')
+			.from(checkoutC, 0.2, {y:-15, autoAlpha:0, ease:Power1.easeOut},'-=0.2')
+			.add('sides2')
+			.from(dotsC2, 0.2, {y:15, autoAlpha:0, ease:Power1.easeOut},'sides2')
+			.from(addC, 0.2, {y:15, autoAlpha:0, ease:Power1.easeOut},'sides2')
+	
+	},
+	methods: function addItem(){
+
+	}
 
 })
