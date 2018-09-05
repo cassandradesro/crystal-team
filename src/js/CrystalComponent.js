c58589458f945aac077ff2e768f2be8e749dad40
cassandradesro <cdesro10@gmail.com>
2018-09-05 11:14:07 -0400

    update to hematite and app.js, all data in , data slider works now as well

--- src/js/CrystalComponent.js
+++ src/js/CrystalComponent.js
@@ -4,6 +4,14 @@ var CrystalComponent = Vue.component("crystal-page", {
 	template: `
 
 		<section class="page" v-bind:style="{ backgroundColor: crystal.backgroundColor, backgroundImage: crystal.backgroundTexture }">
+			<nav>
+				<router-link to="/amethyst"><span id="nav-a" class="circle"></span></router-link>
+				<router-link to="/citrine"><span  id="nav-c"class="circle"></span></router-link>
+				<router-link to="/jasper"><span id="nav-j" class="circle"></span></router-link>
+				<router-link to="/greenfluorite"><span id="nav-f" class="circle"></span></router-link>
+				<router-link to="/rosequartz"><span id="nav-r" class="circle"></span></router-link>
+				<router-link to="/hematite"><span id="nav-h" class="circle"></span></router-link>
+			</nav>
 
 			<!--display flex on .page, with flex-direction: row-->
 			<a href="javascript:;"><img class="bag-icon" src="dist/img/bag.png"></a>
@@ -32,39 +40,37 @@ var CrystalComponent = Vue.component("crystal-page", {
 				<div class="chakras">
 					<div class="chakra-area">
 						<h3>{{crystal.chakraName}}</h3>
-						<img class="chakra-shape" v-bind:src="crystal.chakraImage[counter]" alt="chakra shape">
+						<img class="chakra-shape" v-bind:src="crystal.chakraImage" alt="chakra shape">
 						<h4 v-bind:style="'color: ' + crystal.homeh1Color">{{crystal.chakraNickname}}</h4>
 					</div>
 					<div class="chakra-area" v-if="crystal.chakra2Name">
 						<h3>{{crystal.chakra2Name}}</h3>
-						<img class="chakra-shape" v-bind:src="crystal.chakra2Image[counter]" alt="chakra shape">
+						<img class="chakra-shape" v-bind:src="crystal.chakra2Image" alt="chakra shape">
 						<h4 v-bind:style="'color: ' + crystal.homeh1Color">{{crystal.chakra2Nickname}}</h4>
 					</div>
 				</div>
 				<p class="hide-on-mobile">{{crystal.description}}</p>
 			</div>
-			<div   @click="move(-1)" id="prev" class="hide-on-desktop">
+			<div @click="prev(1)" id="prev" class="hide-on-desktop">
 				<span v-bind:style="'color: ' + crystal.homeh1Color"></span>
 			</div>
 			<div class="column-2">
 
 				<div class="numbers">
-					<img class="active number slide" v-if="index == active" v-bind:src="crystal.numberImages[index]" alt="number one">
+					<img v-for="(numberImage, index) in crystal.numberImages"  v-if="index == crystalSlideshowActive" v-bind:src="numberImage" alt="number one">
 
 				</div>
 				<div class="crystals">
-					<img class="active crystal" v-if="index == active" v-bind:src="crystal.crystalImages[index]" alt="crystal">
+					<img v-for="(crystalImage, index) in crystal.crystalImages" v-if="index == crystalSlideshowActive" v-bind:src="crystalImage" alt="crystal">
 
 				</div>
 				<ul class="dots">
-					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="active dot"></div>
-					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
-					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
-					<div v-on:click="jump(index)" v-bind:style="'border-color: ' + crystal.homeh1Color" class="dot "></div>
+					<div v-for="(crystalImage, index) in crystal.crystalImages" v-bind:style="(index == crystalSlideshowActive)?'border: 2px solid ' + crystal.homeh1Color : '' " v-on:click="jump(index)" ></div>
+
 				</ul>
 
 			</div> <!--function module to updates numbers, crystals and dots simultaneously-->
-			<div  @click="move(1)" id="next" class="hide-on-desktop">
+			<div  @click="next(1)" id="next" class="hide-on-desktop">
 				<span v-bind:style="'color: ' + crystal.homeh1Color"></span>
 			</div>		
 			<div class="column-3">
@@ -87,22 +93,47 @@ var CrystalComponent = Vue.component("crystal-page", {
 			<div class="add-button" v-bind:style="'background-color: ' + crystal.backgroundColor">add</div> <!--position absolute-->
 			
 		</section>
-	`
-	data:{
-		slides: 5,
-		active: 1,
+	`,
+	data: () => {
+		return {
+			crystalSlideshowActive: 0,
+			intervalID: null
+		}
+	},
+	created: function() {
+		// if (window.innerWidth < 768) return; //this code prevents the function from running on mobile
+		this.intervalID = setInterval(() => {
+			console.log("slide changed:" )
+			this.crystalSlideshowActive ++;
+			if (this.crystalSlideshowActive > this.crystal.crystalImages.length - 1){
+				this.crystalSlideshowActive = 0;
+			}
+		}, 8000)
+
 	},
 	methods: {
-	    move(amount) {
-	      let newActive
-	      const newIndex = this.active + amount
-	      if (newIndex > this.slides) newActive = 1
-	      if (newIndex === 0) newActive = this.slides
-	      this.active = newActive || newIndex
-	    },
 	    jump(index) {
-	      this.active = index
+	    	console.log("jump: ", index)
+	    	clearInterval(this.intervalID)
+	      	this.crystalSlideshowActive = index
+
 	    },
+	    next(amount){
+	    	console.log("next")
+	    	clearInterval(this.intervalID)
+	    	this.crystalSlideshowActive ++;
+    		if (this.crystalSlideshowActive > this.crystal.crystalImages.length - 1){
+    			this.crystalSlideshowActive = 0;
+    		}
+	    },
+	    prev(amount){
+	    	console.log("prev")
+	    	clearInterval(this.intervalID)
+	    	this.crystalSlideshowActive --;
+    		if (this.crystalSlideshowActive < 0){
+    			this.crystalSlideshowActive = this.crystal.crystalImages.length - 1;
+    		}
+	    }
 	}
 })
 
